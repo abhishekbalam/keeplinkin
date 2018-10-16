f02e70ac64ee88098be0ca554400a803965a61f9
abhishekbalam <abhishekbalam@gmail.com>
2018-10-16 17:56:29 +0530

    Tue Oct 16 15:30:14 IST 2018

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,8 @@
 
 function validURL(str) {
-  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-    '(\#[-a-z\d_]*)?$','i'); // fragment locater
-  if(!pattern.test(str)) {
-    // alert("Please enter a valid URL.");
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if(!regex .test(str)) {
+    alert("Please enter valid URL.");
     return false;
   } else {
     return true;
