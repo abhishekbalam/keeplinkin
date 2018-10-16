4603671fbbdc34f139eaaa4017a2a0f1bb252078
abhishekbalam <abhishekbalam@gmail.com>
2018-10-16 17:46:10 +0530

    Tue Oct 16 15:30:14 IST 2018

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,35 @@
 
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    // alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
 function stoppedTyping(obj) {
   if(obj.value.length > 0) {
-    console.log(obj.value.indexOf("keeplink.in"));
-    
-    if(obj.value.indexOf("keeplink.in")>=0){
-      $('#error2').css("display","block")
-      $('#modalbtn').prop('disabled',true);
+
+    if(validURL(obj.value)){
+      console.log(obj.value.indexOf("keeplink.in"));
+      if(obj.value.indexOf("keeplink.in")>=0){
+        $('#error2').css("display","block")
+        $('#modalbtn').prop('disabled',true);
+      }
+      else{
+        $('#modalbtn').prop('disabled',false);
+      }
     }
-    else{
-      $('#modalbtn').prop('disabled',false);
+    else{ 
+       $('#error3').css("display","block")
+      $('#modalbtn').prop('disabled',true);
     }
   }
   else{
