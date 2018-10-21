7936d794e620bcb6e8aecd91464557766f9d576a
abhishekbalam <abhishekbalam@gmail.com>
2018-10-21 15:13:23 +0530

    Sun Oct 21 15:02:04 IST 2018

--- shortener.py
+++ shortener.py
@@ -10,22 +10,22 @@ db=redis.from_url(os.environ['REDISCLOUD_URL'])
 # db=redis.Redis(host='localhost', port=6379, password='')
 
 def default(l_url):
-	score=(db.zcard("default")+1)
+	score=(db.zcard('default')+1)
 	
 	if(score<10000):
 		score=score+10000
 
 	s_url=b62.encode(score)
 	
-	db.zadd("default",l_url,score)
-
+	# db.zadd('default',l_url,score)
+	db.execute_command('ZADD', 'default', 'NX', score, l_url)
 	return s_url
 
 def custom(l_url, c_url):
-	status=db.hexists("custom",c_url)
+	status=db.hexists('custom',c_url)
 
 	if(status==0):
-		status=db.hsetnx("custom", c_url, l_url)
+		status=db.hsetnx('custom', c_url, l_url)
 	
 	return c_url
 	
@@ -33,24 +33,24 @@ def semantic(l_url):
 
 	c_url=fw.gen_word(2,4)
 
-	status=db.hexists("custom",c_url)
+	status=db.hexists('custom',c_url)
 
 	if(status==0):
-		status=db.hsetnx("custom", c_url, l_url)
+		status=db.hsetnx('custom', c_url, l_url)
 	else:
 		semantic(l_url)
 			
 	return c_url
 
 def decode(s_url):
-	status=db.hexists("custom",s_url)
+	status=db.hexists('custom',s_url)
 	
 	if(status==1):
-		l_url=db.hget("custom", s_url)
+		l_url=db.hget('custom', s_url)
 	else:
 		try:
 			key=b62.decode(s_url)
-			l_url=db.zrangebyscore("default",key,key)
+			l_url=db.zrangebyscore('default',key,key)
 			l_url=''.join(l_url)
 		except:
 			l_url=False
